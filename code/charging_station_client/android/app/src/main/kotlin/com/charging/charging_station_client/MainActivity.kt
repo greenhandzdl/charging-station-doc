6023fd02dfa866decb237b1e9fe335cbbece7c5e
greenhandzdl <greenhandzdl@gmail.com>
2026-05-30 14:07:08 +0000

    fix: backend_class_diagram StatisticsController/analytics/charges 权限修正为 ADMIN/SUPER_ADMIN

--- code/charging_station_client/android/app/src/main/kotlin/com/charging/charging_station_client/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.charging.charging_station_client
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- code/charging_station_client/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:charging_station_client/main.dart';
-
-void main() {
-  testWidgets('App renders login screen', (WidgetTester tester) async {
-    await tester.pumpWidget(const ChargingStationApp());
-    expect(find.text('充电站管理系统'), findsOneWidget);
-  });
-}
